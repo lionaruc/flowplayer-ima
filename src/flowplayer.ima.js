8c89e36eafe7c95f0ca4390840c0ccabf9d452af
frkcn <frkcn@bil.omu.edu.tr>
2015-10-29 22:28:13 +0200

    google ima html5 sdk Full-Slot Overlay ad format added

--- src/flowplayer.ima.js
+++ src/flowplayer.ima.js
@@ -1,5 +1,5 @@
 /**
- * IMA SDK plugin for Flowplayer HTML5.
+ * IMA SDK integration plugin for Flowplayer HTML5.
  * For more information see https://www.github.com/angelfish-io/flowplayer-ima
  *
  * Copyright (C) 2015 Angelfish AS - Istanbul
@@ -651,6 +651,11 @@ var angVideo = function(conf){
          */
         this.flowplayerUI = ".fp-ui .fp-controls, .fp-ui .fp-time, .fp-ui .fp-embed, .fp-ui .fp-fullscreen";
         /**
+         * is mobile?
+         */
+        this.isMobile = false;
+        this.isAndroid = false;
+        /**
          * initialize plugin with player
          * @param player
          * @param ads
@@ -663,6 +668,15 @@ var angVideo = function(conf){
                 mouseoutTimeout: 5000
             }, function(e, api, video){
             });
+
+            if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+                || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) this.isMobile = true;
+
+
+            var ua = navigator.userAgent.toLowerCase();
+            if(ua.indexOf("android") > -1) {
+                this.isAndroid = true;
+            }
 
             $('.fp-ui .fp-fullscreen, .fp-ui .fp-embed').hide();
             $(".fp-ui").removeAttr("title");
@@ -763,9 +777,9 @@ var angVideo = function(conf){
         this.onPlayWrapperClick = function () {
             if(self.isVideoStarted == false){
                 if(self.prerollAds){
+                    self.adType = self.adMode.preroll;
                     self.requestAds(self.prerollAds.adTagUrl);
                     self.isVideoStarted = true;
-                    self.adType = self.adMode.preroll;
                 }
             }
 
@@ -854,8 +868,8 @@ var angVideo = function(conf){
         this.onPlayerCuepoint = function (e, api, cue) {
             if (cue.time == self.overlayAdTime) {
                 if(self.overlayAds){
-                    self.requestAds(self.overlayAds.adTagUrl);
                     self.adType = null;
+                    self.requestAds(self.overlayAds.adTagUrl);
                 }
             }
             else if (cue.time == self.midrollAdTime) {
@@ -863,8 +877,8 @@ var angVideo = function(conf){
                     self.adsManager.destroy();
                 }
                 if(self.midrollAds){
-                    self.requestAds(self.midrollAds.adTagUrl);
                     self.adType = self.adMode.midroll;
+                    self.requestAds(self.midrollAds.adTagUrl);
                 }
             }
         };
@@ -1013,7 +1027,11 @@ var angVideo = function(conf){
                 self.adsManager.destroy();
             }
             if(self.adType == self.adMode.preroll){
-                self.playerObject.play();
+                if(self.isMobile){
+                    self.playerObject.play();
+                } else {
+                    self.videoContent.play();
+                }
             }
             else if(self.adType == self.adMode.postroll){
                 $('.playContainer').show();
@@ -1038,6 +1056,9 @@ var angVideo = function(conf){
             adsRequest.linearAdSlotHeight = self.wrapperSize.height;
             adsRequest.nonLinearAdSlotWidth = self.wrapperSize.width;
             adsRequest.nonLinearAdSlotHeight = self.wrapperSize.height - 40;
+            if(self.adType !== null){
+                adsRequest.forceNonLinearFullSlot = true;
+            }
             self.adsLoader.requestAds(adsRequest)
         };
         /**
@@ -1101,7 +1122,11 @@ var angVideo = function(conf){
             // This function is where you should setup UI for showing ads (e.g.
             // display ad timer countdown, disable seeking, etc.)
             self.videoContentElement.off('ended', self.contentEndedListener);
-            self.videoContent.pause();
+            if(self.isMobile){
+                self.playerObject.pause();
+            } else {
+                self.videoContent.pause();
+            }
             //console.log("content pause");
         };
         /**
@@ -1115,7 +1140,14 @@ var angVideo = function(conf){
                 self.changeVideoStatus(true);
                 self.onAdUserClose();
             } else {
-                self.videoContent.play();
+                if(self.isMobile){
+                    if(self.isAndroid){
+                        $(".fp-ui").trigger("click");
+                    }
+                    self.playerObject.play();
+                } else {
+                    self.videoContent.play();
+                }
             }
             //console.log("content resume");
         };
@@ -1125,11 +1157,17 @@ var angVideo = function(conf){
         this.onAdUserClose = function() {
             if(self.adType == self.adMode.preroll || self.adType == self.adMode.midroll){
                 self.videoContentElement.on('ended', self.contentEndedListener);
-                self.playerObject.play();
+                if(self.isMobile){
+                    if(self.isAndroid){
+                        $(".fp-ui").trigger("click");
+                    }
+                    self.playerObject.play();
+                } else {
+                    self.videoContent.play();
+                }
                 if(self.adsManager){
                     self.adsManager.destroy();
                 }
-                self.hideAdContainer();
                 $('.adBackground').hide();
                 $('.adContainer').find('div').css('z-index', 0);
                 self.playerObject.conf.keyboard = true;
@@ -1141,19 +1179,20 @@ var angVideo = function(conf){
                 if(self.adsManager){
                     self.adsManager.destroy();
                 }
-                self.hideAdContainer();
                 $('.adBackground').hide();
                 $('.adContainer').find('div').css('z-index', 0);
                 self.playerObject.conf.keyboard = false;
                 $(self.flowplayerUI).show();
                 $('.playContainer').show();
             }
+            self.hideAdContainer();
+            self.isAdPlay = false;
         };
         /**
          * clicked ad open new tab and pause video content (for nonlinear ad)
          */
         this.onAdClick = function() {
-            self.videoContent.pause();
+            //self.videoContent.pause();
         };
         /**
          * skipped ad and video content play
